011566c39eb14a4e067756b029cf2d74d6161768
nevermore-base <semion.potapov2016@yandex.ru>
2020-12-07 00:27:59 +0300

    Add new gulp func

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf')
+const rename = require("gulp-rename");
 
 /* Server */
 gulp.task('server', function() {
@@ -16,9 +18,10 @@ gulp.task('server', function() {
 });
 
 /* Styles Compile*/
-gulp.task('sass', function () {
+gulp.task('styles::compile', function () {
     return gulp.src('source/styles/main.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
         .pipe(gulp.dest('build/css'));
 });
 
@@ -32,7 +35,7 @@ gulp.task('templates::compile', function buildHTML() {
 });
 
 /* SpriteSmith */
-gulp.task('sprite', function () {
+gulp.task('sprite', function (cb) {
     const spriteData = gulp.src('source/images/*.png').pipe(spritesmith({
         imgName: 'sprite.png',
         imgPath: '../images/sprite.png',
@@ -40,6 +43,38 @@ gulp.task('sprite', function () {
     }));
     spriteData.img.pipe(gulp.dest('build/images/'));
     spriteData.css.pipe(gulp.dest('source/styles/global/'));
+    cb();
 });
 
-gulp.task('clean')
+/* RimRaf */
+gulp.task('clean', function dell(cb) {
+    return rimraf('build', cb);
+});
+
+/* Copy Fonts */
+gulp.task('copy::fonts', function(){
+   return gulp.src('./source/fonts/**/*.*')
+       .pipe(gulp.dest('build/fonts'));
+});
+
+/* Copy Images */
+gulp.task('copy::images', function(){
+    return gulp.src('./source/images/**/*.*')
+        .pipe(gulp.dest('build/images'));
+});
+
+/* Copy */
+gulp.task('copy', gulp.parallel('copy::fonts', 'copy::images'));
+
+/* Watchers */
+gulp.task('watch', function () {
+   gulp.watch('source/template/**/*.pug', gulp.series('templates::compile'));
+    gulp.watch('source/styles/**/*.scss', gulp.series('styles::compile'));
+});
+
+/* All Functions in 1 cmd */
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel('templates::compile', 'styles::compile', 'sprite', 'copy'),
+    gulp.parallel('watch', 'server')
+));
